1bd4af9077819a0e98bb70155d75d4f6a1f3c69a
Jones Durumsky <jonesdurumsky@gmail.com>
2021-10-29 16:23:50 +0200

    where... just where

--- src/game.js
+++ src/game.js
@@ -1,7 +1,48 @@
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp() {}
+
+  moveDown() {}
+
+  moveLeft() {}
+
+  moveRight() {}
+
+  draw() {
+    image(this.playerImage, 100, 100, 100, 100);
+    console.log("where is the guy");
+  }
+}
+
 class Game {
+  constructor() {
+    this.player = new Player(10, 10);
+    console.log(player);
+  }
+
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    for (let i = 0; i <= 10; i++) {
+      let x = i * SQUARE_SIDE;
+      stroke(179, 128, 0);
+      line(0, x, WIDTH, x);
+      line(x, 0, x, WIDTH);
+    }
+  }
+
+  preload() {
+    this.playerImage = loadImage("/assets/character-down.png");
+  }
+  draw() {
+    this.player.draw();
   }
 }
+
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 const game = new Game();
 
+
+
+function preload(){
+  game.preload();
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  //game.setup()
 }
 
 function draw() {
   game.drawGrid();
+  game.draw()
+  //player.draw();
 }
