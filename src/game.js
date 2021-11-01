034663d3d189a4555846f59e9e372a4533f8401b
Jones Durumsky <jonesdurumsky@gmail.com>
2021-11-01 09:05:33 +0100

    4th iteration done

--- src/game.js
+++ src/game.js
@@ -1,26 +1,16 @@
-class Player {
-  constructor(col, row) {
-    this.col = col;
-    this.row = row;
-  }
-
-  moveUp() {}
-
-  moveDown() {}
-
-  moveLeft() {}
-
-  moveRight() {}
-
-  draw() {
-    image(game.playerImage, 100, 100, 100, 100);
-    
-  }
-}
-
 class Game {
   constructor() {
-    this.player = new Player(10, 10);
+    this.player = new Player(0, 0);
+    this.treasure = new Treasure(0, 0);
+  }
+
+  preload() {
+    this.playerImage = loadImage("assets/character-down.png");
+    this.playerUp = loadImage("assets/character-up.png");
+    this.playerLeft = loadImage("assets/character-left.png");
+    this.playerRight = loadImage("assets/character-right.png");
+    this.playerDown = loadImage("assets/character-down.png");
+    this.treasureImg = loadImage("assets/treasure.png");
   }
 
   drawGrid() {
@@ -35,13 +25,72 @@ class Game {
       line(x, 0, x, WIDTH);
     }
   }
+}
 
-  preload() {
-    this.playerImage = loadImage("assets/character-down.png");
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.score = 0;
+    
   }
-  draw() {
-    this.player.draw();
+
+  moveUp() {
+    this.col <= 0 ? this.col = 0 : this.col--;
+    game.playerImage = game.playerUp;
+    game.treasure.treasureFound()
+  }
+
+  moveDown() {
+    this.col >= 9 ? this.col = 9 : this.col +=1;
+    game.playerImage = game.playerDown;
+    game.treasure.treasureFound()
+  }
+
+  moveLeft() {
+    this.row  <= 0 ? this.row = 0 : this.row--,
+    game.playerImage = game.playerLeft;
+    game.treasure.treasureFound()
+  }
+
+  moveRight() {
+    this.row >= 9 ? this.row = 9 : this.row++;
+    game.playerImage = game.playerRight;
+    game.treasure.treasureFound()
+  }
+
+  drawPlayer() {
+    image(game.playerImage, this.row * 100, this.col * 100, 100, 100);
   }
 }
 
+class Treasure {
+  setRandomPosition(col, row) {
+    this.row = Math.floor((Math.random() * WIDTH) / 100);
+    this.col = Math.floor((Math.random() * WIDTH) / 100);
+  }
 
+  drawTreasure() {
+    image(game.treasureImg, this.row * 100, this.col * 100, 100, 100);
+  }
+
+  treasureFound() {
+    // console.log(game.player.score)
+    // console.log(game.player.row)
+    const vikingScore = document.querySelector('#vikings-score')
+    if (this.row === game.player.row && this.col === game.player.col) {
+      this.setRandomPosition();
+      game.player.score++;
+      
+      //console.log(vikingScore)
+      vikingScore.innerText = game.player.score
+    }
+    if (game.player.score >= 10){
+      //console.log('you win')
+      const gameOver = document.createElement('h3');
+      gameOver.innerText = 'Well done, oh you viking King!';
+      vikingScore.appendChild(gameOver);
+
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,56 @@
 const game = new Game();
 
-
-
-function preload(){
+function preload() {
   game.preload();
 }
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
-  //game.setup()
+
+  game.treasure.setRandomPosition();
+  //to make sure player and treasure do not start at the same position:
+  while (
+    game.treasure.col === game.player.col &&
+    game.treasure.row === game.player.row
+  ) {
+    game.treasure.setRandomPosition();
+  }
+
+  game.treasure.treasureFound();
 }
 
 function draw() {
+  clear();
   game.drawGrid();
-  game.draw()
-  //player.draw();
+  game.player.drawPlayer();
+  game.treasure.drawTreasure();
 }
+
+function keyPressed() {
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+  if (keyCode === 39) {
+    game.player.moveRight();
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+  }
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+}
+
+//WHY IS THIS FUNCTION NOT EVEN LOGGIN?
+//Shouldn't all functions in the main be activated?
+
+// function treasureFound() {
+//   console.log('TEST');
+//   if (
+//     game.player.row === game.treasure.row &&
+//     game.player.col === game.treasure.col
+//   )   {
+//   return game.player.score++ && game.treasure.setRandomPosition();
+// }
+//}
